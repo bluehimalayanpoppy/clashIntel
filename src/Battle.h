46cd40932db0635c1181aae8cd1a82e50cc13bbf
bluehimalayanpoppy <rushakundu20@gmail.com>
2026-08-06 02:42:48 -0400

    organizes json

---
+++ src/Battle.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include <vector>
+
+class Battle
+{
+public:
+
+    //stores whether this battle was a win or loss
+    bool won;
+
+    //player trophy information before/around the battle
+    int myTrophies;
+    int opponentTrophies;
+
+    //number of crowns each player earned
+    int myCrowns;
+    int opponentCrowns;
+
+    //cards used by each player
+    //vector because the amount of cards can be stored dynamically
+    std::vector<std::string> myDeck;
+    std::vector<std::string> opponentDeck;
+
+    //average level of cards in each deck
+    //used later as numerical data for analysis
+    double myAverageCardLevel;
+    double opponentAverageCardLevel;
+
+
+    //constructor
+    //creates a battle object with default values
+    Battle();
+
+};
